7d59bf8946797e38733865f0d307aabc385b8bb2
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-07-06 23:22:34 -0300

    initial config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,57 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+const ContentSecurityPolicy = `
+   default-src 'self';
+   img-src 'self' data:;
+   script-src 'self' 'unsafe-eval';
+   connect-src 'self' vitals.vercel-insights.com;
+   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+   font-src 'self' https://fonts.gstatic.com;  
+ `
+
+const securityHeaders = () => [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+]
+
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['es-AR'],
+    defaultLocale: 'es-AR',
+    localeDetection: false,
+  },
+  pwa: {
+    disable: process.env.NODE_ENV !== 'production',
+    dest: 'public',
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders(),
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/_document.tsx
@@ -0,0 +1,58 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* ---------------------------------------------------------------------- */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600;700;900&display=swap"
+            rel="stylesheet"
+          />
+          {/* -------------------------------------------------------------------- */}
+          <meta
+            name="description"
+            content="Build a product list with a sample database of products (real products!) we use here in Siempre en Casa."
+          />
+          <meta name="theme-color" content="#333333" />
+          <meta name="referrer" content={'strict-origin'} />
+
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+          <link
+            href="/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
