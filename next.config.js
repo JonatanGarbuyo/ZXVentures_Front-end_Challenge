502988a26519a2d2ec722aa6ae4f983d0c70d65d
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-07-07 12:04:13 -0300

    add linter config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')
+// const withPWA = require('next-pwa')
+
 const runtimeCaching = require('next-pwa/cache')
 
 const ContentSecurityPolicy = `
