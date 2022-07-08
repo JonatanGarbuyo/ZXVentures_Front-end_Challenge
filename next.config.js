bdb8e19665fb286c4eb1bed32ecf62e28a7ad4a8
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-07-08 14:29:21 -0300

    add styled-components

--- next.config.js
+++ next.config.js
@@ -33,6 +33,33 @@ const securityHeaders = () => [
 ]
 
 const nextConfig = {
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: true,
+    /*  | {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+      displayName?: boolean,
+      // Enabled by default.
+      ssr?: boolean,
+      // Enabled by default.
+      fileName?: boolean,
+      // Empty by default.
+      topLevelImportPaths?: string[],
+      // Defaults to ["index"].
+      meaninglessFileNames?: string[],
+      // Enabled by default.
+      cssProp?: boolean,
+      // Empty by default.
+      namespace?: string,
+      // Not supported yet.
+      minify?: boolean,
+      // Not supported yet.
+      transpileTemplateLiterals?: boolean,
+      // Not supported yet.
+      pure?: boolean,
+    } */
+  },
   reactStrictMode: true,
   i18n: {
     locales: ['es-AR'],
