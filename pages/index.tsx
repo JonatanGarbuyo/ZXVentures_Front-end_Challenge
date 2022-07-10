c20c7044dd1c0268a3454c49c2dae45560a6e8d3
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-07-10 15:58:09 -0300

    add base page

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+import Header from '../components/Header'
+import { Container } from '../components/styles/Container.styled'
+
+const Index: NextPage = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Front-end Challenge</title>
+        <meta
+          content="Build a product list with a sample database of products (real products!) we use here in Siempre en Casa."
+          name="description"
+        />
+        <link href="/favicon.ico" rel="icon" />
+      </Head>
+      <Header />
+
+      {/* navigation */}
+
+      <main></main>
+
+      <footer></footer>
+    </Container>
+  )
+}
+
+export default Index
