c1491692cfd5c2ecbd56391da9b1d442a70621a5
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-07-28 20:56:00 -0300

    fix import statements

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 
 import { ProductItem } from '../types'
 
-import { CardButton } from './styles/productCard/CardButton.styled'
 import { CardContainer } from './styles/productCard/CardContainer.styled'
-import { CardHeading } from './styles/productCard/CardHeading.styled'
 import { ImageContainer } from './styles/ImageContainer.styled'
+import { CardHeading } from './styles/productCard/CardHeading.styled'
+import { CardButton } from './styles/productCard/CardButton.styled'
 
 interface Props {
   product: ProductItem
