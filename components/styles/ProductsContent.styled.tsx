09f18c2af07a7ae83b4ed5514f3c0bfa6c5f5e07
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-07-28 20:26:39 -0300

    fix media queries for ProductsContent

--- components/styles/ProductsContent.styled.tsx
+++ components/styles/ProductsContent.styled.tsx
@@ -2,9 +2,5 @@ import styled from 'styled-components'
 
 export const ProductsContent = styled.div`
   min-height: 200px;
-  margin-top: 1rem;
-
-  @media (min-width: 768px) {
-    margin-top: 2rem;
-  }
+  margin-top: 5rem;
 `
